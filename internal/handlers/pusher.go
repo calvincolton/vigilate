bc3c164b9662c41af5438e2200f376863bd57b6c
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-02-05 13:11:11 -0800

    boilerplate+

---
+++ internal/handlers/pusher.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/pusher/pusher-http-go"
+)
+
+func (repo *DBRepo) PusherAuth(w http.ResponseWriter, r *http.Request) {
+	userID := repo.App.Session.GetInt(r.Context(), "userID")
+
+	u, err := repo.DB.GetUserById(userID)
+	if err != nil {
+		log.Println(err)
+	}
+
+	params, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	presenceData := pusher.MemberData{
+		UserID: strconv.Itoa(userID),
+		UserInfo: map[string]string{
+			"name": u.FirstName,
+			"id":   strconv.Itoa(userID),
+		},
+	}
+
+	response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(response)
+}
+
+func (repo *DBRepo) TestPusher(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]string)
+	data["message"] = "Hello, world!"
+
+	err := repo.App.WsClient.Trigger("public-channel", "test-event", data)
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ internal/models/models.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	// ErrNoRecord no record found in database error
+	ErrNoRecord = errors.New("models: no matching record found")
+	// ErrInvalidCredentials invalid username/password error
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// ErrDuplicateEmail duplicate email error
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+	// ErrInactiveAccount inactive account error
+	ErrInactiveAccount = errors.New("models: Inactive Account")
+)
+
+// User model
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	UserActive  int
+	AccessLevel int
+	Email       string
+	Password    []byte
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   time.Time
+	Preferences map[string]string
+}
+
+// Preference model
+type Preference struct {
+	ID         int
+	Name       string
+	Preference []byte
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+// Host is the model for hosts
+type Host struct {
+	ID            int
+	HostName      string
+	CanonicalName string
+	URL           string
+	IP            string
+	IPV6          string
+	Location      string
+	OS            string
+	Active        int
+	CratedAt      time.Time
+	UpdatedAt     time.Time
+}
+
+// Services is the model for services
+type Services struct {
+	ID          int
+	ServiceName string
+	Active      int
+	Icon        string
+	CratedAt    time.Time
+	UpdatedAt   time.Time
+}
+
+// HostServices is the model for host services
+type HostServices struct {
+	ID             int
+	HostID         int
+	ServiceID      int
+	Active         int
+	ScheduleNumber int
+	ScheduleUnit   string
+	Status         string
+	LastCheck      time.Time
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
